0e5b5bf574df307a53136aa1d849fde51623e8ad
tatioca <luckymonkeyb@gmail.com>
2023-10-05 11:54:30 +0200

    create tree

---
+++ exo4_arbo.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+mkdir -p public/{css/{footer,header,main},img,js} && touch index.html && touch public/css/footer/style.css && touch public/css/header/style.css && touch public/css/main/style.css && touch public/img/img1,img2 && touch public/js/main.js
